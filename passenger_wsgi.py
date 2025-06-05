09819083c4d85ddcc629c5f32a63ea9edd78114c
sallari <sallari.ir@gmail.com>
2025-06-06 01:04:44 +0330

    modify wsgi from server

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,16 +1,18 @@
-import os
 import sys
+import os
 
 # اضافه کردن مسیر پروژه به sys.path
-INTERP = os.path.expanduser("~/virtualenv/ferdowsihosseini/3.9/bin/python")
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
-
 sys.path.insert(0, os.path.dirname(__file__))
 
 from app import create_app
 from ssl_config import SSLConfig
 
-application = create_app('production')
-SSLConfig.init_app(application)
-SSLConfig.configure_proxy(application) 
+application = create_app()
+
+# فعال‌سازی تنظیمات SSL فقط اگر متغیر محیطی ENABLE_SSL تنظیم شده باشد
+if os.environ.get('ENABLE_SSL') == 'true':
+    SSLConfig.init_app(application)
+    SSLConfig.configure_proxy(application)
+
+if __name__ == '__main__':
+    application.run() 
